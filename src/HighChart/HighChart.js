f5861e55d23f53b85040995ff89fdf37c78dc139
kirancodezz <95230039+kirancodezz@users.noreply.github.com>
2022-03-02 15:09:52 +0530

    marker radis change

--- src/HighChart/HighChart.js
+++ src/HighChart/HighChart.js
@@ -208,7 +208,7 @@ const HighChrt = () => {
                                                     "#1499AD",
                                 className: "markerShadow",
                                 marker: {
-                                    radius: 9
+                                    radius: 9.5
                                 }
                             })
                             console.log(alertThresholds.lowRed)
